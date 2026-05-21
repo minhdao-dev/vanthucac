5d4fca5d0a28e0b098ed88c151c8c5ae07b1abf9
Dao Nguyen <daonguyenminh.it@gmail.com>
2026-05-21 10:38:51 +0700

    chore: initial project setup

---
+++ src/main/java/com/vanthucac/VanthucacApplication.java
@@ -0,0 +1,13 @@
+package com.vanthucac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanthucacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanthucacApplication.class, args);
+	}
+
+}
